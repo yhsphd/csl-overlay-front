21f6ef30af293a42ee1424f41208b31680c0d847
Yoon Hyungseok <yhsphd05@gmail.com>
2024-12-25 23:53:22 +0900

    [control] Initialize control panel

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/control",
+      name: "control",
+      component: () => import("../views/ControlView.vue"),
+    },
   ],
 });
 
---
+++ src/views/ControlView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, computed } from "vue";
+import { useOverlayDataStore } from "@/stores/socket";
+import { JsonViewer } from "vue3-json-viewer";
+import "vue3-json-viewer/dist/index.css";
+
+const state = useOverlayDataStore();
+const overlayDataSampled = ref({});
+
+setInterval(() => {
+  overlayDataSampled.value = Object.assign(overlayDataSampled.value, state.data);
+}, 1000);
+</script>
+
+<template>
+  <div class="master-control-view">
+    <h1>Circles in SEOUL</h1>
+    <h2>Main event overlay</h2>
+    <h3>CONTROL PANEL</h3>
+    <div>
+      <JsonViewer :value="overlayDataSampled" :expandDepth="4"></JsonViewer>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.master-control-view {
+  width: 1920px;
+  height: 1080px;
+}
+</style>
